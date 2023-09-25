66fb114c863f651ea02cc3580bdbef35549550e0
Anthony Allison <139900552+AnthonyAll181@users.noreply.github.com>
2023-09-25 13:55:41 +0200

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,53 @@
+        console.log("Random Number: " + item);
+
+}
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+    data.forEach(item => {
+    const data = generateRandomData();
+    });
+
+}
+main();
+    return data;
+    for (let i = 0; i < 10; i++) {
+function main() {
+function generateRandomData() {
+    }
+    const data = [];
+        for (int item : data) {
+        }
+        for (int i = 0; i < 10; i++) {
+            data[i] = (int) (Math.random() * 100) + 1;
+            System.out.println("Random Number: " + item);
+
+public class RandomDataGenerator {
+        int[] data = new int[10];
+    }
+        }
+    public static void main(String[] args) {
+}
+    main()
+    for item in data:
+def main():
+
+import random
+
+if __name__ == "__main__":
+    return data
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+def generate_random_data():
+        }
+public class RandomDataGenerator {
+
+        for (int i = 0; i < 10; i++) {
+        for (int item : data) {
+        int[] data = new int[10];
+            System.out.println("Random Number: " + item);
+}
+        }
+            data[i] = (int) (Math.random() * 100) + 1;
+    }
+    public static void main(String[] args) {
