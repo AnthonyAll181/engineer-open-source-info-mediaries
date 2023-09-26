6c92f4e9c7116fc77b9937646ce3f6720b2556e5
Anthony Allison <139900552+AnthonyAll181@users.noreply.github.com>
2023-09-26 07:57:10 +0200

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,53 +1,40 @@
-        console.log("Random Number: " + item);
 
-}
-        data.push(Math.floor(Math.random() * 100) + 1);
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+if __name__ == "__main__":
 
-    data.forEach(item => {
-    const data = generateRandomData();
-    });
-
-}
-main();
-    return data;
-    for (let i = 0; i < 10; i++) {
-function main() {
-function generateRandomData() {
-    }
-    const data = [];
-        for (int item : data) {
-        }
-        for (int i = 0; i < 10; i++) {
-            data[i] = (int) (Math.random() * 100) + 1;
-            System.out.println("Random Number: " + item);
-
-public class RandomDataGenerator {
-        int[] data = new int[10];
-    }
-        }
-    public static void main(String[] args) {
-}
-    main()
     for item in data:
+    main()
+    return data
+
 def main():
+        print(f"Random Number: {item}")
 
 import random
-
-if __name__ == "__main__":
-    return data
+    data = generate_random_data()    for item in data:
     data = [random.randint(1, 100) for _ in range(10)]
+
+    main()
+
+
+def main():
+
         print(f"Random Number: {item}")
     data = generate_random_data()
+import random
+    return data
 def generate_random_data():
-        }
-public class RandomDataGenerator {
+if __name__ == "__main__":def generate_random_data():
 
-        for (int i = 0; i < 10; i++) {
-        for (int item : data) {
-        int[] data = new int[10];
-            System.out.println("Random Number: " + item);
-}
-        }
-            data[i] = (int) (Math.random() * 100) + 1;
-    }
-    public static void main(String[] args) {
+
+if __name__ == "__main__":
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
+    data = generate_random_data()
+    main()
+        print(f"Random Number: {item}")
+import random
+
+def main():
+    return data
